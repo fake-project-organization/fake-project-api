b276581b7f3f8b8347912ccee43a6a96f3b2771c
Tomasz Owczarek <tomasz.owczarek@tomtom.com>
2021-06-28 14:06:32 +0200

    Init code

---
+++ src/main/java/pl/sda/fakeproject/fakeproject/FakeProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.fakeproject.fakeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/fakeproject/fakeproject/FakeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.fakeproject.fakeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
